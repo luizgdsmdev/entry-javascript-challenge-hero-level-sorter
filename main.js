05e1e51915055b7be8cb371a54fc85f5936bed93
Luiz Messias <luizgdsmdev@gmail.com>
2025-08-01 16:12:16 -0300

    Adding switch statements as second option

--- main.js
+++ main.js
@@ -32,5 +32,49 @@ function IfStatementHeroLevelSorter(name, xp){
 
     return `The Hero named ${heroName} is at the level of ${level}.`
 }
+//console.log(IfStatementHeroLevelSorter("Batman", 70000));
 
-console.log(IfStatementHeroLevelSorter("Batman", 80000));
+
+// Sorter using switch statements
+function switchtatementHeroLevelSorter(name, xp){
+    let heroName = name;
+    let level;
+
+    switch (true) {
+        case (xp >= 1001 && xp <= 2000):
+            level = "Bronze";
+            break;
+        
+        case (xp >= 2001 && xp <= 5000):
+            level = "Silver";
+            break;
+        
+        case (xp >= 5001 && xp <= 7000):
+            level = "Gold";
+            break;
+        
+        case (xp >= 7001 && xp <= 8000):
+            level = "Platinum";
+            break;
+
+        case (xp >= 8001 && xp <= 9000):
+            level = "Ascendant";
+            break;
+
+        case (xp >= 9001 && xp <= 10000):
+            level = "Immortal";
+            break;
+
+        case (xp >= 10001):
+            level = "Radiant";
+            break;
+
+        default:
+            level = "Iron";
+            break;
+    }
+
+    return `The Hero named ${heroName} is at the level of ${level}.`
+}
+
+console.log(switchtatementHeroLevelSorter("Batman", 7000));
